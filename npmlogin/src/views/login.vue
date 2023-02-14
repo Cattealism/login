fd5168025fad21d790816151aa2466d4faca0084
高英男 <gaoyingnan@gaoyingnans-MacBook-Air.local>
2023-02-14 21:57:59 +0800

    Initial commit

---
+++ npmlogin/src/views/login.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="container">
+<!--  表单容器-->
+  <div class="forms-container">
+<!--    -->
+    <div class="signin-signup">
+
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style scoped>
+
+</style>
